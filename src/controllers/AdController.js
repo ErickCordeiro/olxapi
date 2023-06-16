97725dc5a7c154367ede716b895bd9fd7b7ccf41
Erick Cordeiro <erick.arruda.ext@infracommerce.com.br>
2023-06-16 20:55:50 -0300

    Created function for add ads

--- src/controllers/AdController.js
+++ src/controllers/AdController.js
@@ -1,9 +1,27 @@
-const getCategories = async (req, res) => {
+import Category from '../models/Category.js';
+import Ad from '../models/Ad.js'
+import User from '../models/User.js';
+import Helper from '../helpers/helpers.js';
 
+const getCategories = async (req, res) => {
+   const cats = await Category.find();
+
+   let categories = [];
+
+   for(let i in cats) {
+      categories.push({
+         ...cats[i]._doc, 
+         img: `${process.env.BASE_URL}/images/${cats[i].slug}.png`
+      });
+   }
+
+   return res.status(200).json({
+      categories
+   })
 };
 
 const index = async (req, res) => {
-    
+ 
 };
 
 const show = async (req, res) => {
@@ -11,7 +29,67 @@ const show = async (req, res) => {
 };
 
 const store = async (req, res) => {
-    
+   let { title, price, priceneg, description, category } = req.body;
+   const token = Helper.getToken(req);
+
+   const user = await User.findOne({
+      token
+   }).exec();
+
+   if(!title || !price || !category){
+      return res.status(404).json({ 
+         error: true,
+         message: "Título, categoria e preço são obrigatório, por favor verifique!"
+      });
+   }
+
+   price = Helper.formatNumber(price);
+
+   try {
+      const newAd = new Ad();
+      newAd.status = true;
+      newAd.userId = user._id;
+      newAd.state = user.state;
+      newAd.category = category;
+      newAd.createdAt = new Date();
+      newAd.title = title;
+      newAd.slug = Helper.strToSlug(title);
+      newAd.description = description;
+      newAd.price = price;
+      newAd.priceNegotible = (priceneg == 'true')? true: false;
+      newAd.views = 0;
+
+      if(req.files && req.files.img){
+         if(req.files.img.lenght == undefined){
+            if(['image/jpeg', 'image/jpg', 'image/png'].includes(req.files.img.mimetype)) {
+               let url = Helper.saveImage(req.files.img.data);
+               newAd.images.push({
+                  url,
+                  default: false
+               });
+            }
+         } else {
+            for(let i=0; i < req.files.img.lenght; i++) {
+               if(['image/jpeg', 'image/jpg', 'image/png'].includes(req.files.img[i].mimetype)) {
+                  let url = Helper.saveImage(req.files.img[i].data);
+                  newAd.images.push({
+                     url,
+                     default: false
+                  });
+               }
+            }
+         }
+      }
+
+      if(newAd.images.length > 0) {
+         newAd.images[0].default = true;
+      }
+
+      const info = await newAd.save();
+      return res.status(201).json({info})
+   } catch (error) {
+      return new Error("ERROR: " + error.message);
+   }
 };
 
 const update = async (req, res) => {
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,8 +1,37 @@
+import { v4 as uuid } from 'uuid';
+import Jimp from 'jimp';
+
 const getToken = (req) => {
     const [authType, token] = req.headers.authorization.split(' ');
     return token;
 }
 
+const formatNumber = (item) => {
+    return parseFloat(item.replace(".", " ").replace(",", "."))
+}
+
+function strToSlug(text) {
+    // Converter para minúsculas
+    text = text.toLowerCase();
+    
+    // Substituir caracteres especiais e espaços por hífens
+    text = text.replace(/[^\w\s-]/g, '');
+    text = text.replace(/[\s]+/g, '-');
+    
+    return text;
+}
+
+const saveImage = async (buffer) => {
+    let newImage = `${uuid()}.jpg`;
+    let tmpImg = await Jimp.read(buffer);
+    tmpImg.cover(500, 500).quality(80).write(`./public/images/ads/${newImage}}`);
+
+    return newImage;
+}
+
 export default {
-    getToken
+    getToken,
+    formatNumber,
+    strToSlug,
+    saveImage
 };
